b92191c58a3b0f6c3fed263022270431ffe95379
Jasmine <jasminehweying@gmail.com>
2023-05-28 13:48:17 +0800

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Jasmine Lai Hweeying on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
